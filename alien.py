f950df16ac7b5e9891a4c8d64fb0089f309e838e
Eryk <erykkurkowski.praca@gmail.com>
2023-04-23 18:29:27 +0200

    Przesunięcie obcych w prawo, zdefinowanie ustawień dla kierunku poruszania się floty oraz sprawdzenie czy obcy dotarł do krawędzi ekranu

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
 	def __init__(self, ai_game):
 		super().__init__()
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 
 		#Wczytywanie obrazu obcego i zdefiniowanie jego atrybutu rect.
 		self.image = pygame.image.load('images/alien.bmp')
@@ -14,7 +15,21 @@ class Alien(Sprite):
 
 		#Umieszczenie nowego obcego w pobliżu lewego górnego rogu ekranu.
 		self.rect.x = self.rect.width
-		self.rect.y - self.rect.height
+		self.rect.y = self.rect.height
 
 		#Przechowywanie dokładnego poziomu położenia obceg.
 		self.x = float(self.rect.x)
+
+	def check_edges(self):
+		"""Zwraca wartość True. jeśli obcy znajduje się przy krawędzi ekranu."""
+		screen_rect = self.screen.get_rect()
+		if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+			return True
+
+	def update(self):
+		"""Przesunięcie obcego w prawo lub lewo."""
+		self.x += (self.settings.alien_speed * self.settins.fleet_direction)
+		
+		self.rect.x = self.x
+
+
